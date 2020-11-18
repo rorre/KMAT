2d853db673180f2c6e059a2bf9540f2e6cbd85f1
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-18 16:20:21 +0700

    HTTP error pages

--- kmat/__init__.py
+++ kmat/__init__.py
@@ -44,11 +44,15 @@ def create_app(config_file="config.json"):
     app.register_blueprint(submission.blueprint)
     app.register_blueprint(user.blueprint)
 
+    @app.errorhandler(403)
+    def unauthorized(e):
+        return render_template("403.html"), 403
+
     @app.errorhandler(404)
     def not_found(e):
         return render_template("404.html"), 404
 
-    @app.errorhandler(505)
+    @app.errorhandler(500)
     def internal_error(e):
         return render_template("500.html"), 500
 
