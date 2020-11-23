4509d3ab226d46ff86efa045015861e0ac49bc61
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-23 15:40:12 +0700

    Add staff page

--- kmat/routes/base.py
+++ kmat/routes/base.py
@@ -1,3 +1,4 @@
+from kmat.models import User
 from flask import Blueprint, render_template
 
 blueprint = Blueprint("base", __name__)
@@ -11,3 +12,15 @@ def index():
 @blueprint.route("/informasi")
 def info():
     return render_template("pages/information.html", title="Informasi")
+
+
+@blueprint.route("/staff")
+def staff():
+    staffs = User.query.filter(User.roles.any(admin=True)).all()
+    judges = User.query.filter(User.roles.any(judge=True)).all()
+    return render_template(
+        "pages/staff.html",
+        title="Staff",
+        staffs=staffs,
+        judges=judges,
+    )
