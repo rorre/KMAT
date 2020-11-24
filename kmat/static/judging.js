1eb26ec3a45a613b81cd3960618e2a0bad89da5d
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-11-25 06:55:33 +0700

    Fix flash + add load indicator upon save

--- kmat/static/judging.js
+++ kmat/static/judging.js
@@ -1,21 +1,28 @@
+const $spinner = $('<span class="spinner-border spinner-border-sm mb-1 mr-2" role="status" aria-hidden="true"></span>')
+
 function flash(message, type) {
     if (type === undefined || type === null) {
         type = "primary";
     }
 
-    $("#flashes").append(```
+    $("#flashes").append(`
     <div class="alert alert-${type}" role="alert">
         ${message}
     </div>
-    ```);
+    `);
 }
 
-$(".save-button").on('click', function () {
-    var $form = $(this).parent().siblings(".modal-body").find("form");
+$(".save-button").on('click', function (event) {
+    event.preventDefault();
+    var $this = $(this);
+
+    var $form = $this.parent().siblings(".modal-body").find("form");
     if (!$form[0].checkValidity()) {
         $form.addClass("was-validated")
         return;
     }
+    $this.prop("disabled", true);
+    $this.prepend($spinner);
     var formArray = $form.serializeArray();
 
     var formJSON = {
@@ -32,11 +39,16 @@ $(".save-button").on('click', function () {
     });
 
     var submissionid = $form.find("input[name='submissionId']").val();
-    axios.post(submissionid, formJSON).then(function (response) {
-        if (response.status != 200) {
-            flash("An error has occured.", "danger");
-        }
-
-        $(this).parent("modal").modal("hide");
-    });
+    axios.post(submissionid, formJSON)
+        .then(function () { flash("Done!"); })
+        .catch(function (response) {
+            if (response.status != 200) {
+                flash("An error has occured.", "danger");
+            }
+        })
+        .finally(function () {
+            $this.parents(".modal").modal("hide");
+            $this.find($spinner).remove();
+            $this.prop("disabled", false);
+        });
 });
